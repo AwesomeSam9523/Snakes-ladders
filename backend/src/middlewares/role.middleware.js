1bc31e47d27bfb4d4ed5a979f02e6eec5bc5f149
Tanmoy mandal <tanmaymandal1250@gmail.com>
2025-12-18 20:36:09 +0530

    Add role middleware

---
+++ backend/src/middlewares/role.middleware.js
@@ -0,0 +1,47 @@
+const { ROLES, MESSAGES } = require('../config/constants');
+const { sendForbidden } = require('../utils/response.util');
+
+/**
+ * Check if user has required role
+ */
+const requireRole = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return sendForbidden(res, MESSAGES.UNAUTHORIZED);
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return sendForbidden(res, MESSAGES.FORBIDDEN);
+    }
+
+    next();
+  };
+};
+
+/**
+ * Participant only middleware
+ */
+const participantOnly = requireRole(ROLES.PARTICIPANT);
+
+/**
+ * Admin only middleware
+ */
+const adminOnly = requireRole(ROLES.ADMIN);
+
+/**
+ * Superadmin only middleware
+ */
+const superadminOnly = requireRole(ROLES.SUPERADMIN);
+
+/**
+ * Admin or Superadmin middleware
+ */
+const adminOrSuperadmin = requireRole(ROLES.ADMIN, ROLES.SUPERADMIN);
+
+module.exports = {
+  requireRole,
+  participantOnly,
+  adminOnly,
+  superadminOnly,
+  adminOrSuperadmin,
+};

a888d20c50b0b03f25653872f14887e3cbb5dacc
Tanmoy mandal <tanmaymandal1250@gmail.com>
2025-12-20 22:24:21 +0530

    Added checkpoint// board // leaderboard routes

---
+++ backend/src/modules/participant/participant.routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const participantController = require('./participant.controller');
+const { verifyToken } = require('../../middlewares/session.middleware');
+const { participantOnly } = require('../../middlewares/role.middleware');
+const { checkDiceLock } = require('../../middlewares/lock.middleware');
+
+// All routes require authentication and participant role
+router.use(verifyToken);
+router.use(participantOnly);
+
+// Dashboard
+router.get('/dashboard', participantController.getDashboard);
+
+// Team state
+router.get('/state', participantController.getTeamState);
+
+// Dice roll
+router.post('/dice/roll', checkDiceLock, participantController.rollDice);
+router.get('/dice/can-roll', participantController.checkCanRollDice);
+
+// Checkpoints
+router.get('/checkpoints', participantController.getCheckpoints);
+
+// Board
+router.get('/board', participantController.getBoard);
+
+// Leaderboard
+router.get('/leaderboard', participantController.getLeaderboardData);
+
+module.exports = router;
